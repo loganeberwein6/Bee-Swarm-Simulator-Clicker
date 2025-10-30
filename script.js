d8b03c25d035b97eec9eae4b10f39c63e0363d94
loganeberwein6 <loganeberwein6@gmail.com>
2025-10-30 01:06:23 +0000

    Fixes for displaying and updating pollen text

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let pollen = 0;
 let pollenPerClick = 1;
-document.getElementById('click-bee').addEventListener('click', () => {
+document.getElementById('clicker').addEventListener('click', () => {
     pollen += 1
 });
+document.getElementById("pollen").textContent = pollen
