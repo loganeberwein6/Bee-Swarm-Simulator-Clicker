a6e1e0bcfbc1715f24527309d1fffbc701d2f031
loganeberwein6 <loganeberwein6@gmail.com>
2025-10-31 02:55:43 +0000

    Fixed pollen text display

--- script.js
+++ script.js
@@ -3,4 +3,5 @@ let pollenPerClick = 1;
 document.getElementById('clicker').addEventListener('click', () => {
     pollen += 1
 });
-document.getElementById("pollen").textContent = pollen
+let pollenText = document.getElementById("pollen");
+pollenText.textContent = pollen;
