9204e386129ba726db223b4df5964a2a5109b4e2
loganeberwein6 <loganeberwein6@gmail.com>
2025-10-31 03:00:39 +0000

    Added default value of 0 to pollen display Fixed pollen text not updating upon click

--- script.js
+++ script.js
@@ -2,6 +2,5 @@ let pollen = 0;
 let pollenPerClick = 1;
 document.getElementById('clicker').addEventListener('click', () => {
     pollen += 1
+    document.getElementById("pollen").textContent = pollen;
 });
-let pollenText = document.getElementById("pollen");
-pollenText.textContent = pollen;
